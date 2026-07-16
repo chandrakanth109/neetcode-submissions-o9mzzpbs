61ff547b2854f769714fb0018547ab18b58c0499
Mallela Chandrakanth <57110355+chandrakanth109@users.noreply.github.com>
2026-07-16 22:51:19 +0530

    Add: longest-increasing-subsequence - submission-3

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-3.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> ans;
+
+        ans.push_back(nums[0]);
+        for(int i=1;i<nums.size();i++) {
+            if(ans.back() < nums[i]) {
+                ans.push_back(nums[i]);
+            } else {
+                int ind=lower_bound(ans.begin(),ans.end(),nums[i])-ans.begin();
+                ans[ind]=nums[i];
+            }
+        }
+
+        return ans.size();
+
+    }
+};
